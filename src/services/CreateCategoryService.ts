eb5b4ceeb06467321962cbdfed514a0d02fc8ce8
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-01 14:27:00 -0300

    creating category

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,27 @@
+import { getRepository } from 'typeorm';
+
+import Category from '../models/Category';
+
+class CreateCategoryService {
+  public async execute(title: string): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: { title },
+    });
+
+    if (checkCategoryExists) {
+      return checkCategoryExists;
+    }
+
+    const category = await categoryRepository.create({
+      title,
+    });
+
+    await categoryRepository.save(category);
+
+    return category;
+  }
+}
+
+export default CreateCategoryService;
