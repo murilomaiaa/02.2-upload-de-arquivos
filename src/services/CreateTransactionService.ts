114b12404cb559ca0c88ecc3e4cfbe2cbbf867cf
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-01 13:45:27 -0300

    Creating a new transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,49 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_id: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('title must be "income" or "outcome"');
+    }
+
+    if (value <= 0) {
+      throw new AppError('value must be greater than 0');
+    }
+
+    if (
+      type === 'outcome' &&
+      value > (await transactionsRepository.getBalance().total)
+    ) {
+      throw new AppError('insufficient funds');
+    }
+
+    // Create an instance of Transaction
+    const transaction = await transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
